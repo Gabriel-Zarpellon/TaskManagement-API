3901b94aa8eed7787bf910733ba84fe41894c81d
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2024-08-30 17:03:06 -0300

    update

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,15 @@
 import express, { json } from "express";
+import helmet from "helmet";
+import "express-async-errors";
+import { taskRouter } from "./routes/task.routes";
+import { HandleError } from "./middlewares/handleError.middleware";
 
 export const app = express();
 
 app.use(json());
+
+app.use(helmet());
+
+app.use("/tasks", taskRouter);
+
+app.use(HandleError.execute);
