17b9c9820e5ef207ded1b5063fad0c800533d2ba
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2024-09-11 17:15:34 -0300

    feat: add condition for dealing with jsonwebtoken errors

--- src/middlewares/handleError.middleware.ts
+++ src/middlewares/handleError.middleware.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { AppError } from "../errors/appError";
 import { ZodError } from "zod";
+import { JsonWebTokenError } from "jsonwebtoken";
 
 export class HandleError {
   static execute(
@@ -12,8 +13,10 @@ export class HandleError {
     if (error instanceof AppError) {
       return res.status(error.statusCode).json({ message: error.message });
     } else if (error instanceof ZodError) {
-      const data = {errors: error.issues}
+      const data = { errors: error.issues };
       return res.status(400).json(data);
+    } else if (error instanceof JsonWebTokenError) {
+      return res.status(401).json({ message: error.message });
     } else {
       console.log(error);
       return res.status(500).json({ message: "Internal server error." });
