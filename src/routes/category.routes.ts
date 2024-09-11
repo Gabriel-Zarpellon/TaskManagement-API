4f4cdfa37b92882dfdf8934dacd8ca69cf3153c0
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2024-09-11 17:18:45 -0300

    feat: add validation middlewares to routes

--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -6,16 +6,19 @@ import { CategoryControllers } from "../controllers/category.controllers";
 import { ValidateBody } from "../middlewares/validateBody.middleware";
 import { categoryBodySchema } from "../schemas/category.schema";
 import { IsCategoryIdValid } from "../middlewares/isCategoryIdValid.middleware";
+import { ValidateCategoryOwner } from "../middlewares/validateCategoryOwner.middleware";
+import { ValidateToken } from "../middlewares/validateToken.middleware";
 
 export const categoryRouter = Router();
 
 container.registerSingleton("CategoryServices", CategoryServices);
 const categoryControllers = container.resolve(CategoryControllers);
 
+categoryRouter.use(ValidateToken.execute);
 categoryRouter.post("/", ValidateBody.execute(categoryBodySchema), (req, res) =>
   categoryControllers.create(req, res)
 );
 
-categoryRouter.delete("/:id", IsCategoryIdValid.execute, (req, res) =>
+categoryRouter.delete("/:id", IsCategoryIdValid.execute, ValidateCategoryOwner.execute, (req, res) =>
   categoryControllers.delete(req, res)
 );
--- src/routes/task.routes.ts
+++ src/routes/task.routes.ts
@@ -7,6 +7,9 @@ import { ValidateBody } from "../middlewares/validateBody.middleware";
 import { taskBodySchema, taskUpdateSchema } from "../schemas/task.schema";
 import { IsTaskIdValid } from "../middlewares/isTaskIdValid.middleware";
 import { IsBodyCategoryValid } from "../middlewares/isBodyCategoryValid.middleware";
+import { ValidateToken } from "../middlewares/validateToken.middleware";
+import { ValidateTaskOwner } from "../middlewares/validateTaskOwner.middleware";
+
 
 
 export const taskRouter = Router();
@@ -14,8 +17,9 @@ export const taskRouter = Router();
 container.registerSingleton("TaskServices", TaskServices);
 const taskControllers = container.resolve(TaskControllers);
 
+taskRouter.use(ValidateToken.execute);
 taskRouter.post("/", ValidateBody.execute(taskBodySchema), IsBodyCategoryValid.execute, (req, res) => taskControllers.create(req, res));
 taskRouter.get("/", (req, res) => taskControllers.findMany(req, res));
-taskRouter.get("/:id", IsTaskIdValid.execute, (req, res) => taskControllers.findOne(req, res));
-taskRouter.patch("/:id", IsTaskIdValid.execute, ValidateBody.execute(taskUpdateSchema), (req, res) => taskControllers.update(req, res));
-taskRouter.delete("/:id", IsTaskIdValid.execute, (req, res) => taskControllers.delete(req, res));
+taskRouter.get("/:id", IsTaskIdValid.execute, ValidateTaskOwner.execute, (req, res) => taskControllers.findOne(req, res));
+taskRouter.patch("/:id", IsTaskIdValid.execute, ValidateTaskOwner.execute, ValidateBody.execute(taskUpdateSchema), (req, res) => taskControllers.update(req, res));
+taskRouter.delete("/:id", IsTaskIdValid.execute, ValidateTaskOwner.execute, (req, res) => taskControllers.delete(req, res));
